a476679e633eb5955325c32dfabae6d7f6b3811f
Kyial <116427081+kyial05@users.noreply.github.com>
2022-12-19 12:44:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeClassList();
+    panel.classList.add("active");
+  });
+});
+function removeClassList() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
